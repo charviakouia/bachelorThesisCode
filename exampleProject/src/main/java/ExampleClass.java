65eaf493a0cb0d7e00f84abf253d43b4029c05d0
charviakouia <ivancharviakou@gmail.com>
2022-05-15 09:21:42 +0200

    Created project

---
+++ exampleProject/src/main/java/ExampleClass.java
@@ -0,0 +1,8 @@
+
+public class ExampleClass {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+}
---
+++ exampleProject/src/test/java/ExampleClassTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleClassTest {
+
+    @Test
+    public void testAdd(){
+
+        Assertions.assertEquals(ExampleClass.add(1, 3), 4);
+
+    }
+
+    @Test
+    public void uselessTest(){
+
+        int sillyExpression = 73 % 10 + 3;
+        Assertions.assertEquals(sillyExpression, 6);
+
+    }
+
+}
